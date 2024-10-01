f163ff0003b7d031dc4aa596ac920037bd2d19f1
Alexandre De Carvalho Pimentel <apimentel@firjan.com.br>
2024-10-01 16:04:20 -0300

    Conectando a API

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-const url = "https://hp-api.onrender.com/api/characters";
+const harry = "https://hp-api.onrender.com/api/characters";
+let listaDeAmigos = document.getElementById("listaDeAmigos");
+
+let lista = [];
+
+function convertPersonagemToLi(personagem){
+    return `
+    <li class="amigo">
+        <h3>${personagem.name}</h3>
+        <p>Ator: ${personagem.actor}</p>
+        <p>Casa: ${personagem.house}</p>
+    </li>
+    `
+}
+
+
+fetch(harry)
+    .then((response) => response.json())
+    .then((jsonresponse) => listaDeAmigos.innerHTML += jsonresponse.map(convertPersonagemToLi).join(""))
+    .catch((error) => console.log(error))
 
